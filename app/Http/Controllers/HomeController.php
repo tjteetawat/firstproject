aeb0475cefe01c5ee4395ef348691f64153a555b
TJ Teetawat <teetawat1809@gmail.com>
2020-10-06 18:37:03 +0700

    change password

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Hash;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -25,4 +28,41 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function password(){
+
+        $user = auth()->user();
+
+        return view('password_change');
+
+    }
+
+    public function changepassword(Request $request){
+
+        $req =  $request->all();
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+            return redirect()->back()->with("error","Your current password does not matches with the password you provided. Please try again.");
+        }
+
+        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+            //Current password and new password are same
+            return redirect()->back()->with("error","New Password cannot be same as your current password. Please choose a different password.");
+        }
+
+        $validatedData = $request->validate([
+            'current-password' => 'required',
+            'new-password' => 'required|string|min:5|confirmed',
+        ]);
+
+        //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new-password'));
+        $user->save();
+
+        return redirect()->back()->with("success","Password changed successfully !");
+
+
+    }
+
+
 }
